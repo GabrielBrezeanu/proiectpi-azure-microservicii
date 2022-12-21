e8aafe94545a5ffbf98574dda72fc09b8ac8cf91
f0rest <gbrezeanu@protonmail.com>
2022-12-21 12:39:45 +0200

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = 1337;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'aplicatie cobai - test!!!\n'
+  const msg = 'aplicatie cobai - test!\n'
   res.end(msg);
 });
 
