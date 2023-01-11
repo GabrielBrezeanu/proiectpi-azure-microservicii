97a43b57533da0f224a9092c07490415916e3b47
G Brezeanu <gbrezeanu@protonmail.com>
2023-01-11 11:33:01 +0200

    test

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ io.on("connection", socket => {
     } else {
       socket.join(user.room);
 
-      socket.emit("message", generateMessage("Admin", "Welcome!"));
+      socket.emit("message", generateMessage("Admin", "Welcome"));
       socket.broadcast.to(user.room).emit("message", generateMessage("Admin", `${user.username} has joined!`));
       io.to(user.room).emit("roomData", {
         room: user.room,
